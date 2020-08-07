268af12a1b3083891161dae36ead779f0c1f820e
Brian Hsing <brian.hsing.tw@gmail.com>
2020-08-07 17:11:48 +0800

    Update VMs-Networking-Config.ps1

--- VMs-Networking-Config.ps1
+++ VMs-Networking-Config.ps1
@@ -3,3 +3,4 @@ $adapter = Get-NetAdapter -Name "vEthernet (InternalNAT)"
 New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceIndex $adapter.InterfaceIndex
 New-NetNat -Name "InternalNat" -InternalIPInterfaceAddressPrefix 192.168.0.0/24
 netsh advfirewall firewall add rule name="Open Port for Hyper-V" dir=in action=allow protocol=TCP localport="5985,5986"
+winrm quickconfig
