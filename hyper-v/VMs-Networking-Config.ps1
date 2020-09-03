54246691df510a221ba1adb8e9c6b61a346dea42
Brian Hsing <brian.hsing.tw@gmail.com>
2020-09-03 11:55:13 +0800

    Create VMs-Networking-Config.ps1

---
+++ hyper-v/VMs-Networking-Config.ps1
@@ -0,0 +1,4 @@
+﻿New-VMSwitch -Name "InternalNAT" -SwitchType Internal
+$adapter = Get-NetAdapter -Name "vEthernet (InternalNAT)"
+New-NetIPAddress -IPAddress 192.168.0.1 -PrefixLength 24 -InterfaceIndex $adapter.InterfaceIndex
+New-NetNat -Name "InternalNat" -InternalIPInterfaceAddressPrefix 192.168.0.0/24
